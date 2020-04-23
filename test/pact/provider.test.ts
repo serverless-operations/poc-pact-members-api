564e1e259575faea0d4b38af854e530e6b3a8fff
lopburny <lopburny@riotz.works>
2020-04-23 13:57:25 +0000

    Update pact test options

--- test/pact/provider.test.ts
+++ test/pact/provider.test.ts
@@ -1,5 +1,5 @@
 import { Verifier } from '@pact-foundation/pact'
-import pkg from '../../package.json'
+// import pkg from '../../package.json'
 
 const {
   PROVIDER_BASE_URL,
@@ -13,14 +13,14 @@ describe('Provider test', () => {
     provider: 'poc-pact-members-api',
     providerBaseUrl: PROVIDER_BASE_URL!,
     pactBrokerUrl: PACT_BROKER_URL,
-    tags: [ 'latest' ],
+    consumerVersionTags: [ 'master' ],
     pactBrokerToken: PACT_BROKER_TOKEN,
     publishVerificationResult: true,
-    providerVersion: pkg.version,
+    providerVersion: 'provider-0.0.0'
   }
 
   test('Verify pact', async () => {
-    const result = await new Verifier().verifyProvider(opts)
+    const result = await new Verifier(opts).verifyProvider()
     expect(result).toBeTruthy()
   })
 })
